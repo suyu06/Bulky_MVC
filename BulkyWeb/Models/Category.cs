122bf45d1ddf40c49d999de3dbcd4bc72a46d15f
suyu06 <suyu.zhang@johnabbottcollege.net>
2024-04-03 21:40:41 -0400

    data annotation

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,9 +1,16 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
     public class Category
     {
+        [Key] 
+        // if the property's name is Id or the classname+Id，key annotation will not required.
         public int Id { get; set; }
-        public string Name { get; set; }
+
+        [Required]
+        // means the value of this column could not be null.
+        public string CategoryName { get; set; }
         public int DisplayOrder { get; set; }
    
 
