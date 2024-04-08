3caf214d3ae0e6b9a700432c715511ad49ba3852
suyu06 <suyu.zhang@johnabbottcollege.net>
2024-04-07 22:28:23 -0400

    create Db and table by EntityFramework

---
+++ BulkyWeb/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        // create table Categories in the db Bulky
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
