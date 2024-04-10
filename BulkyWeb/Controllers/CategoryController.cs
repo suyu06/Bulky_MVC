f49c6a48500fa9d5d2bb767d2637d61762356f75
suyu06 <suyu.zhang@johnabbottcollege.net>
2024-04-10 09:21:21 -0400

    get all categories

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        //inject class AppDbContext
+        private readonly AppDbContext _appDbContext;
+        public CategoryController(AppDbContext appDbContext) {
+            _appDbContext = appDbContext;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _appDbContext.Categories.ToList();
             return View();
         }
     }
