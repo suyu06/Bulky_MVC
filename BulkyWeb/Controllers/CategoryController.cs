a8e650192fd68db9b078b7db701f47a6f4ad910a
suyu06 <suyu.zhang@johnabbottcollege.net>
2024-04-19 23:21:01 -0400

    get Id in Edit method of Category Controller

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,9 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq;
+
 
 namespace BulkyWeb.Controllers
 {
@@ -43,5 +46,37 @@ namespace BulkyWeb.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id==null|| id == 0) {
+                return NotFound();
+            }
+            Category? categoryFromDb = _appDbContext.Categories.Find(id);
+            //Category? categoryFromDb2 = _appDbContext.Categories.FirstOrDefault(obj=>obj.Id==id);
+            // Category? categoryFromDb3 = _appDbContext.Categories.Where(obj=>obj.Id == id).FirstOrDefault();
+            if (categoryFromDb==null) {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category newCategory)
+        {
+            if (newCategory.CategoryName == newCategory.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("DisplayOrder",
+                    "the DisplayOrder cannot exactly match the CategroyName");
+            }
+            
+
+            if (ModelState.IsValid)
+            {
+                _appDbContext.Add(newCategory);
+                _appDbContext.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
     }
 }
