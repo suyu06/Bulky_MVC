d3a0d139dc9130a12fa4854a5665d191989f99db
suyu06 <suyu.zhang@johnabbottcollege.net>
2024-04-23 22:46:22 -0400

    create a new category

---
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,28 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _appDbContext;
+        //[BindProperty]
+        public Category Category { get; set; }
+       
+        public CreateModel(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public void OnGet() { }
+        public IActionResult OnPost()
+        {           
+            _appDbContext.Add(Category);
+            _appDbContext.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
