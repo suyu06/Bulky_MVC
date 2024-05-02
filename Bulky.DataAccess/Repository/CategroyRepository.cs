e240afd927429ca2b6fb8c813b2cab3c1d6c1349
suyu06 <suyu.zhang@johnabbottcollege.net>
2024-05-01 21:12:45 -0400

    create  class CategroyRepository implement  Repository<Category> and  InterfaceCategoryRepository

---
+++ Bulky.DataAccess/Repository/CategroyRepository.cs
@@ -0,0 +1,31 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.InterfaceRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class CategroyRepository :Repository<Category>, InterfaceCategoryRepository
+    {
+
+        private AppDbContext _db;
+        public CategroyRepository(AppDbContext db):base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _db.Categories.Update(category);
+        }
+    }
+}
