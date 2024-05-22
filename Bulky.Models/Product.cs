f0d682cf368d037f71e6b0bc0332066308e227c7
suyu06 <suyu.zhang@johnabbottcollege.net>
2024-05-21 22:06:22 -0400

    Add product model to the project

---
+++ Bulky.Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        // if the property's name is Id or the classname+Id，key annotation will not required.
+        public int Id { get; set; }
+
+        [Required]
+        // means the value of this column could not be null.       
+        public string Title { get; set; }
+        [Required]               
+        public string Description { get; set; }
+        [Required]              
+        public string ISBN { get; set; }
+        [Required]           
+        public string Author { get; set; }
+
+        [Required]
+        [Display(Name ="List Price")]
+        [Range(1,1000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 50+")]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+    }
+}
