109f1f37ef973f8af8713836e64c117335ca3a99
suyu06 <suyu.zhang@johnabbottcollege.net>
2024-05-27 21:46:06 -0400

    create InterfaceProductRepository and ProductRepository

---
+++ Bulky.DataAccess/Repository/InterfaceRepository/InterfaceProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.InterfaceRepository
+{
+    public interface InterfaceCategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+        //void Save();
+    }
+}
